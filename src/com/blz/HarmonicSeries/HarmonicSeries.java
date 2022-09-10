ca940cc6c80f5795d0270b3b82fcb6f1eb47406a
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 13:56:23 +0530

    Added: Welcome message

---
+++ src/com/blz/HarmonicSeries/HarmonicSeries.java
@@ -0,0 +1,10 @@
+package com.blz.HarmonicSeries;
+
+public class HarmonicSeries {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to HarmonicSeries Program!!");
+
+	}
+
+}
